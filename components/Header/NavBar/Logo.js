2acd4dc008b65e0e04d1c6983c7abe88a4c1a2d2
Quentin Jacob <73841714+jcbquentin@users.noreply.github.com>
2021-12-21 11:13:25 +0100

    adding link to home in nav logo

--- components/Header/NavBar/Logo.js
+++ components/Header/NavBar/Logo.js
@@ -1,11 +1,12 @@
 import { Box, Text } from '@chakra-ui/react';
+import Link from "next/link"
 
 const Logo = (props) => {
   return(
     // Logo
     <Box {...props}>
       <Text fontSize="lg" fontWeight="bold">
-        Archerynted
+        <Link href="/">Archerynted</Link>
       </Text>
     </Box>
   )
